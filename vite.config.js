666152d7efb36023255d77e8baa258926b2f0f47
shresthabibash810 <sthabibash1000@gmail.com>
2025-08-03 14:47:12 +0545

    Making Project Details Responsive

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     visualizer({ open: true }) // Generates dist/stats.html bundle report
   ],
-  base: process.env.VITE_BASE_PATH || '/BibashPortfolioWebsite',
+  // base: process.env.VITE_BASE_PATH || '/BibashPortfolioWebsite',
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
